28a42146daae14175bdfd00605b9cde0f821c950
shumiao <shumiao@google.com>
2024-03-10 22:48:58 -0700

    Create a main.tf for a fleet with 3 clusters and 2 teams

--- fleet-and-argocd/main.tf
+++ fleet-and-argocd/main.tf
@@ -8,19 +8,6 @@ provider "google" {
   region  = "us-central1"
 }
 
-# Provision a control cluster in the fleet. It is used as the Management Cluster of Argo CD.
-resource "google_container_cluster" "fleet-control-cluster" {
-  name  = "fleet-control-cluster"
-  initial_node_count = 3
-  fleet {
-    project = local.fleet_project
-  }
-  workload_identity_config {
-    workload_pool = "${data.google_project.project.project_id}.svc.id.goog"
-  }
-  deletion_protection = false
-}
-
 # Provision 3 application clusters
 resource "google_container_cluster" "clusters" {
   count = 3
