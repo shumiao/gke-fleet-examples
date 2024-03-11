bfd6c4552f20f8521454a6abe99584a1dfebc1d1
shumiao <shumiao@google.com>
2024-03-10 22:31:31 -0700

    Update main.tf

--- fleet-and-argocd/main.tf
+++ fleet-and-argocd/main.tf
@@ -1,5 +1,11 @@
 locals {
   fleet_project = "shumiao-test"
+  membership_re = "//gkehub.googleapis.com/projects/([^/]*)/locations/([^/]*)/memberships/([^/]*)$"
+}
+
+provider "google" {
+  project = "shumiao-test"
+  region  = "us-central1"
 }
 
 # Provision a control cluster in the fleet. It is used as the Management Cluster of Argo CD.
@@ -13,4 +19,46 @@ resource "google_container_cluster" "fleet-control-cluster" {
     workload_pool = "${data.google_project.project.project_id}.svc.id.goog"
   }
   deletion_protection = false
+}
+
+# Provision 3 application clusters
+resource "google_container_cluster" "clusters" {
+  count = 3
+  name  = "app-cluster-${count.index}"
+  initial_node_count = 3
+  fleet {
+    project = local.fleet_project
+  }
+  # Enable Workload Identity
+  workload_identity_config {
+    workload_pool = "${data.google_project.project.project_id}.svc.id.goog"
+  }
+  deletion_protection = false
+}
+
+# Provision two teams "backend" and "frontend"
+resource "google_gke_hub_scope" "scopes" {
+  for_each = {
+    "backend"  = {}
+    "frontend" = {}
+  }
+  project  = local.fleet_project
+  scope_id = each.key
+}
+
+resource "google_gke_hub_namespace" "frontend_team_namespaces" {
+  project            = local.fleet_project
+  scope_namespace_id = "webserver"
+  scope_id           = "frontend"
+  scope              = google_gke_hub_scope.scopes["frontend"].id
+}
+
+# Bind cluster 1 & 2 to the "frontend" scope
+resource "google_gke_hub_membership_binding" "backend_cluster_bindings" {
+  count                 = 2
+  project               = local.fleet_project
+  membership_binding_id = "to-frontend"
+  scope                 = google_gke_hub_scope.scopes["frontend"].id
+  membership_id         = regex(local.membership_re, google_container_cluster.clusters[count.index].fleet[0].membership)[2]
+  location              = regex(local.membership_re, google_container_cluster.clusters[count.index].fleet[0].membership)[1]
 }
